fb90d213ace2552bcbae5a0fd2ad8d366d77fa36
HardwareTaster <a.artikov@albynschool.org>
2024-10-04 12:08:07 +0100

    Add files via upload

---
+++ # savings plan running total.py
@@ -0,0 +1,12 @@
+# Set total to 0
+total = 0
+# Loop 7 times
+for index in range(12):
+    # Get this months savings from user
+    savings = float(input("Enter this months savings: "))
+    # Add savings to total
+    total = total + savings
+    print("total")
+
+# Display total rounded to 2 decimal places
+print(round(total,2))
