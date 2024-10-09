c7a28aeefabc6cf68c84f82dc63979a877ff2257
HardwareTaster <a.artikov@albynschool.org>
2024-10-09 09:44:12 +0100

    Add files via upload

---
+++ dog food array.py
@@ -0,0 +1,9 @@
+# Set up items array + total variable
+items = [200, 100, 290,234, 60]
+total = 0
+# Loop 7 times
+for index in range(5):
+# Add current item in items array to total
+total = total + items[index]
+# Display total
+print(total)
---
+++ temperatures 1d array.py
@@ -0,0 +1,10 @@
+# Set up items array + total variable
+items = [4,9,6,13,24,25,1]
+total = 0
+# Loop 7 times
+for index in range(7):
+# Add current item in items array to total
+total = total + items[index]
+# Display total
+average = total/7
+print(average)
